9ecdada072312f5b846d0871a8fd528f2d3750b1
Igor Putko <igorpetindev@gmail.com>
2026-07-08 18:31:39 +0300

    a50: Set bootimg flags
    Set bootimg flags required for recovery boot.
    
    Signed-off-by: Igor Putko <igorpetindev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,3 +23,15 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# BootIMG
+BOARD_KERNEL_CMDLINE := \
+    androidboot.hardware=exynos9610 \
+    androidboot.selinux=permissive
+
+BOARD_MKBOOTIMG_ARGS := \
+    --base 0x10000000 \
+    --kernel_offset 0x00008000 \
+    --pagesize 2048 \
+    --ramdisk_offset 0x01000000 \
+    --tags_offset 0x00000100 \
+    --header_version 1
