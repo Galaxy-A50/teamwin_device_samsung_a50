06047ee509cf572a9f135f8d59539aef1432476b
Igor Putko <igorpetindev@gmail.com>
2026-07-08 18:18:39 +0300

    a50: Init a skeleton of BoardConfig.mk
    Add a basic skeleton of BoardConfig.mk to set
    the device path and allow missing dependencies.
    
    Signed-off-by: Igor Putko <igorpetindev@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2026 IgorpetinDev
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Set Device Path
+DEVICE_PATH := device/samsung/a50
+
+# Allow Missing Dependencies
+ALLOW_MISSING_DEPENDENCIES := true
