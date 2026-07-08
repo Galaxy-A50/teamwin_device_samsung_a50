83a564b7f2e2b07adb4d81e38ddb37dd74f5a15e
Igor Putko <igorpetindev@gmail.com>
2026-07-08 18:14:58 +0300

    a50: Create AndroidProducts.mk
    Add an AndroidProducts.mk file to include the twrp_a50.mk file
    and create common twrp lunch choices for A50.
    
    Signed-off-by: Igor Putko <igorpetindev@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2026 IgorpetinDev
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a50.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a50-userdebug \
+    twrp_a50-eng
