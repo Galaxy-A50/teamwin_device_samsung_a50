42d369aed09d16c27da617a61f81eaff3f9587cb
Igor Putko <igorpetindev@gmail.com>
2026-07-08 19:38:34 +0300

    a50: Create device.mk
    Add a device.mk file to configure basic product variables:
    - Set shipping API level to 28 (Android 9)
    - Disable dynamic partitions
    - Add fastbootd to product packages
    - Include android.hardware.health HAL for recovery
    
    Signed-off-by: Igor Putko <igorpetindev@gmail.com>

---
+++ device.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2026 IgorpetinDev
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 28
+
+# Disable dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := false
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
