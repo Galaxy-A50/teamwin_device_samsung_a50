24e28817887c9172505af665d9b62c2ce2a69382
Igor Putko <igorpetindev@gmail.com>
2026-07-08 18:24:10 +0300

    a50: Set architecture flags
    Set architecture flags for Exynos 9610 core config.
    
    Signed-off-by: Igor Putko <igorpetindev@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,3 +9,17 @@ DEVICE_PATH := device/samsung/a50
 
 # Allow Missing Dependencies
 ALLOW_MISSING_DEPENDENCIES := true
+
+# Architecture (Exynos 9610 big cores)
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a73
+
+# Secondary Architecture (Exynos 9610 little cores)
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
